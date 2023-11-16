6b6f03cf23818447db17d186c66ee341e2d982f1
lenaqurom <113707663+lenaqurom@users.noreply.github.com>
2023-11-16 20:10:53 +0200

    Update planning.dart

--- lib/trip/planning.dart
+++ lib/trip/planning.dart
@@ -1,37 +1,32 @@
+import 'dart:io';
+
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
-import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:masarna/navbar/BottomNavBar.dart';
+import 'package:masarna/navbar/animatedbar.dart';
+import 'package:masarna/navbar/rive_asset.dart';
+import 'package:masarna/navbar/rive_utils.dart';
+import 'package:masarna/trip/PlanSearch.dart';
 import 'package:masarna/user/home.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Planning(),
-      theme: ThemeData(
-        primaryColor: Color.fromARGB(255, 39, 26, 99),
-        hintColor: Color.fromARGB(255, 39, 26, 99),
-      ),
-    );
-  }
-}
+import 'package:masarna/user/makeprofile.dart';
+import 'package:rive/rive.dart';
 
 class Planning extends StatefulWidget {
+  const Planning({super.key});
+
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<Planning> createState() => _PlanningState();
 }
 
-class _HomeScreenState extends State<Planning> {
+class _PlanningState extends State<Planning> {
+  RiveAsset selectedBottomNav = bottomNavs.elementAt(2);
   List<Plan> plans = [];
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   File? selectedImage;
   int? selectedPlanIndex;
-  int _currentIndex = 1;
 
   Future<void> pickImage() async {
     final pickedFile =
@@ -43,117 +38,125 @@ class _HomeScreenState extends State<Planning> {
     }
   }
 
-void addPlan() {
-  AwesomeDialog(
-    context: context,
-    dialogType: DialogType.noHeader,
-    animType: AnimType.BOTTOMSLIDE,
-    body: SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            "Add a Plan",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 39, 26, 99)),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextField(
-              controller: titleController,
-              decoration: InputDecoration(
-                labelText: 'Title',
-                labelStyle: TextStyle(color: Color.fromARGB(255, 39, 26, 99)),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
-                ),
-              ),
-              maxLength: 20,
+  void addPlan() {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.noHeader,
+      animType: AnimType.BOTTOMSLIDE,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              "Add a Plan",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 39, 26, 99)),
             ),
-          ),
-          SizedBox(height: 5),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextField(
-              controller: descriptionController,
-              decoration: InputDecoration(
-                labelText: 'Description',
-                labelStyle: TextStyle(color: Color.fromARGB(255, 39, 26, 99)),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
-                ),
-              ),
-              maxLength: 50,
-              maxLines: 3,
+            SizedBox(
+              height: 20,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text("Select image for your plan"),
-              Container(
-                height: 50.0, // Set the maximum height for the image
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal, // Enable horizontal scrolling if needed
-                  child: Row(
-                    children: [
-                      if (selectedImage != null)
-                        Image.file(selectedImage!, height: 100.0),
-                      IconButton(
-                        onPressed: pickImage,
-                        icon: Icon(FontAwesomeIcons.image),
-                        color: Color.fromARGB(255, 39, 26, 99),
-                      ),
-                      // You can add more image selection widgets here
-                    ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: TextStyle(color: Color.fromARGB(255, 39, 26, 99)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
                   ),
                 ),
+                maxLength: 20,
               ),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: TextStyle(color: Color.fromARGB(255, 39, 26, 99)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 39, 26, 99)),
+                  ),
+                ),
+                maxLength: 50,
+                maxLines: 3,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("Select image for your plan"),
+                Container(
+                  height: 50.0, // Set the maximum height for the image
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis
+                        .horizontal, // Enable horizontal scrolling if needed
+                    child: Row(
+                      children: [
+                        if (selectedImage != null)
+                          Image.file(selectedImage!, height: 100.0),
+                        IconButton(
+                          onPressed: pickImage,
+                          icon: Icon(FontAwesomeIcons.image),
+                          color: Color.fromARGB(255, 39, 26, 99),
+                        ),
+                        // You can add more image selection widgets here
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-    btnCancelText: "Cancel",
-    btnCancelIcon: Icons.cancel,
-    btnCancelColor: Color.fromARGB(255, 39, 26, 99),
-    btnCancelOnPress: () {},
-    btnOkText: "Add",
-    btnOkIcon: Icons.add,
-    btnOkColor: Color.fromARGB(255, 39, 26, 99),
-    btnOkOnPress: () {
-      if (selectedPlanIndex == null) {
-        setState(() {
-          plans.add(Plan(
-            title: titleController.text,
-            description: descriptionController.text,
-            image: selectedImage,
-          ));
-        });
-      } else {
-        setState(() {
-          plans[selectedPlanIndex!] = Plan(
-            title: titleController.text,
-            description: descriptionController.text,
-            image: selectedImage,
-          );
-        });
-      }
-      titleController.clear();
-      descriptionController.clear();
-      selectedImage = null;
-      selectedPlanIndex = null;
-    },
-  )..show();
-}
+      btnCancelText: "Cancel",
+      btnCancelIcon: Icons.cancel,
+      btnCancelColor: Color.fromARGB(255, 39, 26, 99),
+      btnCancelOnPress: () {},
+      btnOkText: "Add",
+      btnOkIcon: Icons.add,
+      btnOkColor: Color.fromARGB(255, 39, 26, 99),
+      btnOkOnPress: () {
+        if (selectedPlanIndex == null) {
+          setState(() {
+            plans.add(Plan(
+              title: titleController.text,
+              description: descriptionController.text,
+              image: selectedImage,
+            ));
+          });
+        } else {
+          setState(() {
+            plans[selectedPlanIndex!] = Plan(
+              title: titleController.text,
+              description: descriptionController.text,
+              image: selectedImage,
+            );
+          });
+        }
+        titleController.clear();
+        descriptionController.clear();
+        selectedImage = null;
+        selectedPlanIndex = null;
+      },
+    )..show();
+  }
 
   void editPlan(int index) {
     selectedPlanIndex = index;
@@ -187,29 +190,43 @@ void addPlan() {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(24),
+            bottomRight: Radius.circular(24),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 39, 26, 99).withOpacity(0.5),
         elevation: 0,
         title: Text(
           'Your Plans',
           style: TextStyle(
-            color: Color.fromARGB(255, 39, 26, 99),
+            fontFamily: 'Dosis',
+            color: Color.fromARGB(255, 255, 255, 255),
             fontWeight: FontWeight.bold,
             fontSize: 26,
           ),
         ),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              FontAwesomeIcons.search,
-              color: Color.fromARGB(255, 39, 26, 99),
-            ),
-            onPressed: () {
+          GestureDetector(
+            onTap: () {
               showSearch(
                 context: context,
                 delegate: PlanSearch(plans),
               );
             },
+            child: SizedBox(
+              height: 40,
+              width: 40,
+              child: RiveAnimation.asset(
+                "icons/icons.riv",
+                artboard: "SEARCH",
+              ),
+            ),
           ),
+          SizedBox(
+            width: 15,
+          )
         ],
       ),
       body: Column(
@@ -314,165 +331,80 @@ void addPlan() {
         child: Icon(Icons.add,),
         backgroundColor: Color.fromARGB(255, 39, 26, 99),
       ),
-            bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          if (index == 0) {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-          } else {
-            setState(() {
-              _currentIndex = index;
-            });
-          }
-        },
-        fixedColor: Color.fromARGB(255, 39, 26, 99), 
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: Color.fromARGB(255, 39, 26, 99),
+
+      bottomNavigationBar: SafeArea(
+          child: Container(
+        padding: EdgeInsets.all(12),
+        margin: EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 39, 26, 99).withOpacity(0.5),
+            borderRadius: BorderRadius.all(Radius.circular(24))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ...List.generate(
+              bottomNavs.length,
+              (index) => GestureDetector(
+                  onTap: () {
+                    bottomNavs[index].input!.change(true);
+                    if (bottomNavs[index] != selectedBottomNav) {
+                      setState(() {
+                        if (index == 0) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ));
+                        } else if (index == 1) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ));
+                        } else if (index == 2) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Planning(),
+                          ));
+                        } else if (index == 3) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ProfileApp(),
+                          ));
+                        }
+                        selectedBottomNav = bottomNavs[index];
+                      });
+                    }
+                    Future.delayed(const Duration(seconds: 1), () {
+                      bottomNavs[index].input!.change(false);
+                    });
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AnimatedBar(
+                          isActive: bottomNavs[index] == selectedBottomNav),
+                      SizedBox(
+                          height: 36,
+                          width: 36,
+                          child: Opacity(
+                            opacity: bottomNavs[index] == selectedBottomNav
+                                ? 1
+                                : 0.5,
+                            child: RiveAnimation.asset(
+                              bottomNavs.first.src,
+                              artboard: bottomNavs[index].artboard,
+                              onInit: (artboard) {
+                                StateMachineController controller =
+                                    RiveUtils.getRiveController(artboard,
+                                        StateMachineName:
+                                            bottomNavs[index].stateMachineName);
+                                bottomNavs[index].input =
+                                    controller.findSMI("active") as SMIBool;
+                              },
+                            ),
+                          ))
+                    ],
+                  )),
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.edit_document,
-              color: Color.fromARGB(255, 39, 26, 99),
-            ),
-            label: 'Planning',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.notifications,
-              color: Color.fromARGB(255, 39, 26, 99),
-            ),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: Color.fromARGB(255, 39, 26, 99),
-            ),
-            label: 'Profile',
-          ),
-        ],
-      ),
+          ],
+        ),
+      )),
 
-    );
-  }
-}
-
-class PlanSearchResults extends StatelessWidget {
-  final List<Plan> searchResults;
-
-  PlanSearchResults(this.searchResults);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: searchResults.length,
-      itemBuilder: (context, index) {
-        final plan = searchResults[index];
-        return InkWell(
-          onTap: () {
-        Navigator.of(context).pushNamed("path");
-          },
-          child: ListTile(
-            title: Text(plan.title),
-            subtitle: Text(plan.description),
-        ));
-      },
-    );
-  }
-}
-
-class Plan {
-  String title;
-  String description;
-  File? image;
-
-  Plan({
-    required this.title,
-    required this.description,
-    this.image,
-  });
-}
-
-class PlanSearch extends SearchDelegate<String> {
-  final List<Plan> plans;
-
-  PlanSearch(this.plans);
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      )
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, query);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    final results = plans.where((plan) {
-      return plan.title.contains(query) || plan.description.contains(query);
-    }).toList();
-
-    return PlanSearchResults(results);
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    final suggestionList = plans
-        .where((plan) => plan.title.toLowerCase().contains(query.toLowerCase()))
-        .toList();
-
-    return ListView.builder(
-      itemCount: suggestionList.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(suggestionList[index].title),
-          onTap: () {
-            query = suggestionList[index].title;
-            showResults(context);
-          },
-        );
-      },
-    );
-  }
-}
-
-class PlanDetailScreen extends StatelessWidget {
-  final Plan plan;
-
-  PlanDetailScreen(this.plan);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(plan.title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Title: ${plan.title}'),
-          Text('description: ${plan.description}'),
-        ],
-      ),
     );
   }
 }
