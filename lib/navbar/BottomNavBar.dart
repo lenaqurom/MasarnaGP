e1683e309734fe034fab1d5226e49e62d28f5a8a
lenaqurom <113707663+lenaqurom@users.noreply.github.com>
2023-11-16 20:17:14 +0200

    adding bottom navbar

---
+++ lib/navbar/BottomNavBar.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:masarna/navbar/animatedbar.dart';
+import 'package:masarna/navbar/rive_asset.dart';
+import 'package:masarna/navbar/rive_utils.dart';
+import 'package:masarna/trip/planning.dart';
+import 'package:masarna/user/home.dart';
+import 'package:masarna/user/makeprofile.dart';
+import 'package:masarna/user/profile.dart';
+import 'package:rive/rive.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<BottomNavBar> {
+  RiveAsset selectedBottomNav = bottomNavs.elementAt(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: SafeArea(
+          child: Container(
+        padding: EdgeInsets.all(12),
+        margin: EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 39, 26, 99).withOpacity(0.5),
+            borderRadius: BorderRadius.all(Radius.circular(24))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ...List.generate(
+              bottomNavs.length,
+              (index) => GestureDetector(
+                  onTap: () {
+                    bottomNavs[index].input!.change(true);
+                    if (bottomNavs[index] != selectedBottomNav) {
+                      setState(() {
+                        if (index == 0) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ));
+                        } else if (index == 1) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ));
+                        } else if (index == 2) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Planning(),
+                          ));
+                        } else if (index == 3) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ProfileApp(),
+                          ));
+                        }
+                        selectedBottomNav = bottomNavs[index];
+                      });
+                    }
+                    Future.delayed(const Duration(seconds: 1), () {
+                      bottomNavs[index].input!.change(false);
+                    });
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AnimatedBar(
+                          isActive: bottomNavs[index] == selectedBottomNav),
+                      SizedBox(
+                          height: 36,
+                          width: 36,
+                          child: Opacity(
+                            opacity: bottomNavs[index] == selectedBottomNav
+                                ? 1
+                                : 0.5,
+                            child: RiveAnimation.asset(
+                              bottomNavs.first.src,
+                              artboard: bottomNavs[index].artboard,
+                              onInit: (artboard) {
+                                StateMachineController controller =
+                                    RiveUtils.getRiveController(artboard,
+                                        StateMachineName:
+                                            bottomNavs[index].stateMachineName);
+                                bottomNavs[index].input =
+                                    controller.findSMI("active") as SMIBool;
+                              },
+                            ),
+                          ))
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
